1276ded305a9a49694f38a081e2f94bf248df050
DevendraPatil007 <devendrapatil3087@gmail.com>
2026-06-19 17:47:08 +0530

    day 14

---
+++ react/pratice_react/src/compnets/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Fotter =() =>{
+    return(
+        <>
+        <div>
+            <p> hello guys </p>
+            
+        </div>
+        </>
+    );
+};
+
+export default Navbar ;
